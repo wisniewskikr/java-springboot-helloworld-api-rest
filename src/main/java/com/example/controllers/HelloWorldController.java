823c00a428282dbbf7f2cf18c4afc34628d54a22
Wisniewski <wisniewk@emea.roche.com>
2023-03-07 07:17:58 +0100

    Update project

--- src/main/java/com/example/controllers/HelloWorldController.java
+++ src/main/java/com/example/controllers/HelloWorldController.java
@@ -25,7 +25,7 @@ public class HelloWorldController {
 		this.environment = environment;
 	}
 
-	@RequestMapping(value="/helloworld")
+	@RequestMapping(value="/")
 	public HelloWorldJson helloWorld() {
 				
 		String port = environment.getProperty("local.server.port");
